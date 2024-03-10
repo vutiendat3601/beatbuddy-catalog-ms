a10596c60fa1c5c0b2ea52026f31fac1e3cc988d
vutiendat3601 <vutien.dat.3601@gmail.com>
2024-03-10 23:45:53 +0700

    chore: initial commit

---
+++ src/main/java/vn/io/datvutech/beatbuddy/catalog/CatalogServiceApplication.java
@@ -0,0 +1,15 @@
+package vn.io.datvutech.beatbuddy.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@SpringBootApplication
+public class CatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/io/datvutech/beatbuddy/catalog/CatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.io.datvutech.beatbuddy.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
