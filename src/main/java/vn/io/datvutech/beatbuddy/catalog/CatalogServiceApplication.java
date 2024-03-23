c5ea29694129929347ade109c7fed4180328f045
vutiendat3601 <vutien.dat.3601@gmail.com>
2024-03-23 17:00:46 +0000

    feat: add Server Side Discovery Kubernetes

--- src/main/java/vn/io/datvutech/beatbuddy/catalog/CatalogServiceApplication.java
+++ src/main/java/vn/io/datvutech/beatbuddy/catalog/CatalogServiceApplication.java
@@ -2,9 +2,11 @@ package vn.io.datvutech.beatbuddy.catalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CatalogServiceApplication {
 	public static void main(String[] args) {
