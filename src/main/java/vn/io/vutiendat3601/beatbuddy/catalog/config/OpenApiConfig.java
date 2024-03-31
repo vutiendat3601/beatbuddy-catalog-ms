60e760d21ffb5a1cfd346f33facd8bfc3093b3b4
vutiendat3601 <vutien.dat.3601@gmail.com>
2024-03-31 22:32:45 +0000

    chore: fix title of OpenApi

--- src/main/java/vn/io/vutiendat3601/beatbuddy/catalog/config/OpenApiConfig.java
+++ src/main/java/vn/io/vutiendat3601/beatbuddy/catalog/config/OpenApiConfig.java
@@ -1,13 +1,11 @@
 package vn.io.vutiendat3601.beatbuddy.catalog.config;
 
-import static vn.io.vutiendat3601.beatbuddy.catalog.constant.GlobalConstant.BEAT_BUDDY;
-
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
 @OpenAPIDefinition(
         info = @Info(
-            title = BEAT_BUDDY, version = "1.0.0",
+            title = "BeatBuddy - Catalog APIs", version = "1.0.0",
             description = "BeatBuddy Catalog APIs"
         )
 
